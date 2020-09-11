c93c0eaf3cc859c1a06f05985ab6887b03a521fc
royeradames <royeraadames@gmail.com>
2020-09-11 12:30:10 -0400

    added names for the helper functions

--- projects/projects-module.js
+++ projects/projects-module.js
@@ -1,13 +1,27 @@
 const db = require('../data/db-config.js');
 
 module.exports = {
-    getIngredientForRecipes
+    addResource,
+    getResources,
+    addProject,
+    getProjects,
+    addTask,
+    getTasks
 }
 
 // all recipes in the system that utilize a single ingredient
-function getIngredientForRecipes(ingredient_id) {
+function addResource(resource_id) {
     /*
        
     */
     
 }
+
+/*
+    adding resources.
+    retrieving a list of resources.
+    adding projects.
+    retrieving a list of projects.
+    adding tasks.
+    retrieving a list of tasks. The list of tasks should include the project name and project description.
+*/
