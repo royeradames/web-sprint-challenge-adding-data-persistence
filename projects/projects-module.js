e05e64e64850d5124be800c7cad8971cbddca7be
royeradames <royeraadames@gmail.com>
2020-09-11 12:06:34 -0400

    init module file

---
+++ projects/projects-module.js
@@ -0,0 +1,13 @@
+const db = require('../data/db-config.js');
+
+module.exports = {
+    getIngredientForRecipes
+}
+
+// all recipes in the system that utilize a single ingredient
+function getIngredientForRecipes(ingredient_id) {
+    /*
+       
+    */
+    
+}
