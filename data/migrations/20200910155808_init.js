7dc2ba5b1c7bad6bca19e04805f4c2647bc522f7
royeradames <royeraadames@gmail.com>
2020-09-11 12:00:45 -0400

    completed tasks, and update resources tables

--- data/migrations/20200910155808_init.js
+++ data/migrations/20200910155808_init.js
@@ -5,23 +5,21 @@ exports.up = function (knex) {
             tbl.increments()
             tbl.string('project_name').notNullable()
             tbl.string('description')
-            tbl.integer('project_completed').notNullable().defaultTo(0)
+            tbl.boolean('project_completed').notNullable().defaultTo('false')
         })
         .createTable('resources', tbl => {
             tbl.increments()
-            tbl.string('description').notNullable()
-
+            tbl.text('description').notNullable()
         })
         .createTable('tasks', tbl => {
             tbl.increments()
-            tbl.text('step', 'mediumtext').notNullable()
-            tbl.integer('recipe_id')
-            .unsigned()
-            .notNullable()
-            .references('recipes.id')//shorthand
-            .onDelete('RESTRICT')
-            .onUpdate('CASCADE')
-            tbl.integer('order').notNullable()
+            tbl.string('description').notNullable()
+            tbl.text('notes')
+            tbl.boolean('task_completed').notNullable().defaultTo('false')
+            tbl.string('project_id').notNullable()
+                .references('projects.id')//shorthand
+                .onDelete('RESTRICT')
+                .onUpdate('CASCADE')
         })
         .createTable('projects_resources', tbl => {
             tbl.increments()
@@ -40,7 +38,7 @@ exports.up = function (knex) {
                 .references('ingredients.id')//shorthand
                 .onDelete('RESTRICT')
                 .onUpdate('CASCADE')
-            tbl.float ('quantity').notNullable()
+            tbl.float('quantity').notNullable()
 
         })
 }
@@ -48,8 +46,8 @@ exports.up = function (knex) {
 // drop tables in the opposite way they where added
 exports.down = function (knex) {
     return knex.schema
-    .dropTableIfExists('projects_resources')
-    .dropTableIfExists('tasks')
-    .dropTableIfExists('resources')
-    .dropTableIfExists('projects')
+        .dropTableIfExists('projects_resources')
+        .dropTableIfExists('tasks')
+        .dropTableIfExists('resources')
+        .dropTableIfExists('projects')
 }
