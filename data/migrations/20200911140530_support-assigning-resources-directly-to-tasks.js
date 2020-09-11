2b45008fe72970a715b1dd40931a748e2bafe42f
royeradames <royeraadames@gmail.com>
2020-09-11 14:16:29 -0400

    Added support for assigning resources directly to tasks in addition to adding them to projects

--- data/migrations/20200911140530_support-assigning-resources-directly-to-tasks.js
+++ data/migrations/20200911140530_support-assigning-resources-directly-to-tasks.js
@@ -1,8 +1,23 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    knex.schema.createTable('users', function (table) {
+        table.increments();
+        tbl.integer('resource_id')
+            .unsigned()
+            .notNullable()
+            .references('resources.id')//shorthand
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE')
+        tbl.integer('task_id')
+            .unsigned()
+            .notNullable()
+            .references('tasks.id')//shorthand
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE')
+    })
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    return knex.schema
+    .dropTableIfExists('projects_resources')
 };
