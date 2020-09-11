4ff5f20839d25b3b7256e06f1736142ea323d52b
royeradames <royeraadames@gmail.com>
2020-09-11 13:56:28 -0400

    completed getProjectTasks

--- projects/projects-module.js
+++ projects/projects-module.js
@@ -3,6 +3,7 @@ const db = require('../data/db-config.js');
 module.exports = {
     addProject,
     getProjects,
+    getProjectTasks,
 }
 
 // adding projects.
@@ -20,4 +21,20 @@ function addProject(newProject) {
 //retrieving a list of projects.
 function getProjects() {
     return db('projects')
+}
+
+//  Add an endpoint to get a list of project tasks.
+function getProjectTasks (projectId) {
+    /*
+        select *
+        from projects as p
+        join tasks as t
+            on t.project_id = p.id
+        where p.id = 1
+        ;
+    */
+    return db('projects as p')
+    .join('tasks as t', 't.project_id', 'p.id')
+    .where('p.id', projectId)
+
 }
